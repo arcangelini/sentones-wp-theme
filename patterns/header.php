824f2a3c33620cc5f503beac5a7b6a7979f2eac6
Tony Arcangelini <tony@arcangelini.com>
2025-07-31 21:14:11 -0500

    Fix header

--- patterns/header.php
+++ patterns/header.php
@@ -5,9 +5,6 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"border":{"bottom":{"color":"var:preset|color|secondary","width":"1px"},"top":[],"right":[],"left":[]},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"backgroundColor":"background","layout":{"type":"default"}} -->
-<div class="wp-block-group has-background-background-color has-background" style="border-bottom-color:var(--wp--preset--color--secondary);border-bottom-width:1px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-
 <!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull"><!-- wp:group {"metadata":{"name":"Header wrapper"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide is-layout-flex wp-block-group-is-layout-flex" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)">
@@ -15,6 +12,5 @@
 <div class="wp-sent-ones-logo"><a rel="home" title="Sent Ones" href="https://sentones.com"><?php echo file_get_contents( esc_url( get_template_directory_uri() . '/assets/images/logo.svg' ) ); ?></a></div>
 
 <!-- wp:navigation {"ref":4,"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","justifyContent":"right"}} /--></div>
-<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
