98b1b50ddf04f4fa0292c4d7a99aa80bbc966deb
Tony Arcangelini <tony@arcangelini.com>
2025-08-02 16:27:49 -0500

    Adjust some stuff

--- blocks/categories-section/build/categories-section/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '92b4b9925e92ab953793');
--- blocks/categories-section/categories-section.php
+++ blocks/categories-section/categories-section.php
@@ -33,7 +33,7 @@ function create_block_categories_section_block_init() {
 	 * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
 	 */
 	if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
-		wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
+		wp_register_block_types_from_metadata_collection( __DIR__ . '/../../build', __DIR__ . '/../../build/blocks-manifest.php' );
 		return;
 	}
 
@@ -44,16 +44,16 @@ function create_block_categories_section_block_init() {
 	 * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
 	 */
 	if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
-		wp_register_block_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
+		wp_register_block_metadata_collection( __DIR__ . '/../../build', __DIR__ . '/../../build/blocks-manifest.php' );
 	}
 	/**
 	 * Registers the block type(s) in the `blocks-manifest.php` file.
 	 *
 	 * @see https://developer.wordpress.org/reference/functions/register_block_type/
 	 */
-	$manifest_data = require __DIR__ . '/build/blocks-manifest.php';
+	$manifest_data = require __DIR__ . '/../../build/blocks-manifest.php';
 	foreach ( array_keys( $manifest_data ) as $block_type ) {
-		register_block_type( __DIR__ . "/build/{$block_type}" );
+		register_block_type( __DIR__ . "/../../build/{$block_type}" );
 	}
 }
 add_action( 'init', 'create_block_categories_section_block_init' );
